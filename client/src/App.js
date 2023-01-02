2082dd0185b45d6b03412f1af419e7dddc1f9e67
nsns245 <suginao1234@gmail.com>
2023-01-03 01:39:03 +0900

    create react for client

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Game from './Game.js'
+import Join from './Join.js'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Context from './Context.js'
+
+const App = () => {
+  return (
+    <Context>
+      <BrowserRouter>
+        <Routes>
+          <Route path={`/`} element={<Join />} />
+          <Route path={`/Game`} element={<Game />} />
+        </Routes>
+      </BrowserRouter>
+    </Context>
+  );
+}
+
+export default App;
---
+++ client/src/Join.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import { useContext } from 'react';
+import { GlobalContext } from './Context.js';
+
+const Join = () => {
+    const context = useContext(GlobalContext);
+    const navigate = useNavigate();
+    function joinRoom() {
+        const client_ = context.clientId.replace(/\s+/g, "");
+        const room_ = context.roomId.replace(/\s+/g, "");
+        if (!context.clientId || !context.roomId) {
+            //英数字チェックはできてない
+            alert("何か入力してください");
+            return;
+        } else if (client_!=context.clientId || room_!=context.roomId) {
+            alert("スペースは含まないでください");
+            return;
+        }
+        navigate('/Game')
+    };
+    return (
+        <div className="Join">
+            <h1>Welcome</h1>
+            <p>enter id</p>
+            <input value={context.clientId} onChange={(event) => context.setClientId(event.target.value)} />
+            <p>enter room id</p>
+            <input value={context.roomId} onChange={(event) => context.setRoomId(event.target.value)} />
+            <br></br>
+            
+            <button onClick={joinRoom}>
+                Join Room
+            </button>
+
+        </div>
+    );
+}
+
+export default Join;
