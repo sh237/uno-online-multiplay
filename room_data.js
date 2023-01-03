1b3ccecca374027b16e734bd3d733d1100a5c6e9
sh237 <syun864297531@gmail.com>
2023-01-03 16:28:49 +0900

    Incorporated existing hand field into players_info

--- room_data.js
+++ room_data.js
@@ -3,16 +3,12 @@ const mongoose = require('mongoose');
 const RoomSchema = mongoose.Schema({
     room_name:String,
     deck:[{color:String,special:String,number:Number}],
-    player1:[{color:String,special:String,number:Number}],
-    player2:[{color:String,special:String,number:Number}],
-    player3:[{color:String,special:String,number:Number}],
-    player4:[{color:String,special:String,number:Number}],
     current_field:{color:String,special:String,number:Number},
     number_of_player:Number,
     order:[String],
     is_reverse:Boolean,
     current_player:Number,
-    players_info:[{player_name:String, socket_id:String}],
+    players_info:[{player_name:String, socket_id:String,cards:[{color:String,special:String,number:Number}]}],
     binded_players:[{player_id:String, remain_turn:Number}],
     number_turn_play:Number,//何ターン目か(仕様書に書いてあった)
     number_card_play:Number,//何枚場に出たか(仕様書に書いてあった)
