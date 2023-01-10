fec4fee590153b3d10b23e69657ba10ce73fc9bc
sh237 <syun864297531@gmail.com>
2023-01-10 12:27:29 +0900

    Modified the must_call_draw_card decision process

--- server/socket-io-game.js
+++ server/socket-io-game.js
@@ -21,8 +21,25 @@ module.exports = (io) => {
                 //ここにペナルティ処理の追加
                 return;
               }
+              //このプレイヤーがroom.binded_playersに含まれているかどうか確認、更新する
+              let binded_player = room.binded_players.find((binded_player) => {
+                return binded_player.player_id == player._id;
+              });
+              if(binded_player != null){
+                if(binded_player.remain_turn == 2){
+                  binded_player.remain_turn --;
+                }else if(binded_player.remain_turn == 1){
+                  //このプレイヤーをroom.binded_playersから削除する
+                  room.binded_players = room.binded_players.filter((binded_player) => {
+                    return binded_player.player_id != player._id;
+                  });
+                }
+              }
+
 
               let next_player = false;
+              let is_playable = false;
+
               //場のカードがワイルドドロー4の場合
               if(room.current_field.special == Special.WILD_DRAW_4){
                 //4枚ドローする
@@ -62,7 +79,6 @@ module.exports = (io) => {
                 player.cards.push(draw_card);
 
                 //ドローしたカードが場に出せるかを確認
-                let is_playable = false;
                 if(draw_card.special == Special.WHITE_WILD ||  draw_card.special == Special.WILD_DRAW_4 || draw_card.special == Special.WILD || draw_card.special == Special.WILD_SHUFFLE){
                   is_playable = true;
                 }
@@ -84,7 +100,8 @@ module.exports = (io) => {
                   number_card_of_player[player._id] = player.cards.length;
                 });
                 room.number_turn_play++;
-                io.to(next_player.socket_id).emit(SocketConst.EMIT.NEXT_PLAYER, {next_player:next_next_player._id, before_player:player._id, card_before:room.current_field, card_of_player:next_player.cards, must_call_draw_card:checkMustCallDrawCard(room, room.room_name, next_player._id), draw_reason:DrawReason.NOTING, turn_right:!room.is_reverse,  number_card_play : room.number_card_play, number_turn_play : room.number_turn_play, number_card_of_player : number_card_of_player});
+                let is_must_call_draw_card = (room.current_field.special == Special.DRAW_2 || room.current_field.special == Special.WILD_DRAW_4) ? true : false;
+                io.to(next_player.socket_id).emit(SocketConst.EMIT.NEXT_PLAYER, {next_player:next_next_player._id, before_player:player._id, card_before:room.current_field, card_of_player:next_player.cards, must_call_draw_card: is_must_call_draw_card, draw_reason:DrawReason.NOTING, turn_right:!room.is_reverse,  number_card_play : room.number_card_play, number_turn_play : room.number_turn_play, number_card_of_player : number_card_of_player});
               }
 
               //saveする
@@ -117,7 +134,8 @@ module.exports = (io) => {
                 number_card_of_player[player._id] = player.cards.length;
               });
               room.number_turn_play++;
-              io.to(next_player.socket_id).emit(SocketConst.EMIT.NEXT_PLAYER, {next_player:next_next_player._id, before_player:player._id, card_before:room.current_field, card_of_player:next_player.cards, must_call_draw_card:checkMustCallDrawCard(room, room.room_name, next_player._id), draw_reason:DrawReason.NOTING, turn_right:!room.is_reverse,  number_card_play : room.number_card_play, number_turn_play : room.number_turn_play, number_card_of_player : number_card_of_player});
+              let is_must_call_draw_card = (room.current_field.special == Special.DRAW_2 || room.current_field.special == Special.WILD_DRAW_4) ? true : false;
+              io.to(next_player.socket_id).emit(SocketConst.EMIT.NEXT_PLAYER, {next_player:next_next_player._id, before_player:player._id, card_before:room.current_field, card_of_player:next_player.cards, must_call_draw_card:is_must_call_draw_card, draw_reason:DrawReason.NOTING, turn_right:!room.is_reverse,  number_card_play : room.number_card_play, number_turn_play : room.number_turn_play, number_card_of_player : number_card_of_player});
 
             }
           });
@@ -169,7 +187,7 @@ module.exports = (io) => {
 
     const waitForChangeColor = (room) => {
       return new Promise((resolve, reject) => {
-        let TIMEOUT = 300000;
+        let TIMEOUT = 3000000;
         const timeoutId = setTimeout(() => {
           reject(new Error(`Timed out while waiting for change color event`));
         }, TIMEOUT);
@@ -177,7 +195,7 @@ module.exports = (io) => {
         socket.on(SocketConst.EMIT.COLOR_OF_WILD, (data) => {
           clearTimeout(timeoutId);
           room.current_field.color = data.color_of_wild;
-          
+
           resolve(data);
         });
       });
@@ -202,8 +220,8 @@ module.exports = (io) => {
               return card.color == card_play.color && card.special == card_play.special && card.number == card_play.number;
             });
             if(index != -1){
-              //プレイヤーが持っているカードの中にこのカードがある
-              //カードを場に出す
+              /*プレイヤーが持っているカードの中にこのカードがある場合、
+              カードを場に出す*/
               let previos_color = room.current_field.color;
               room.current_field = card_play;
               room.number_card_play++;
@@ -212,17 +230,6 @@ module.exports = (io) => {
               //出したカードが数字ならなにもしない、特殊なら処理を行う
 
               if(card_play.special == Special.DRAW_2){
-                //2枚ドローする
-                // let draw_card1 = room.deck.shift();
-                // let draw_card2 = room.deck.shift();
-                //ドローしたカードを次のプレイヤーの手札に加える
-                //次のプレイヤーを取得する
-                // let next_player = getNextPlayer(room);
-                // updateCurrentPlayer(room);
-
-                // next_player.cards.push(draw_card1);
-                // next_player.cards.push(draw_card2);
-
                 //カードを場に出したことをクライアントに通知する
                 if(socketEvent == SocketConst.EMIT.PLAY_CARD){
                   //room.uno_declaredにプレイヤーのidがあるか確認し、あれば削除する
@@ -270,9 +277,9 @@ module.exports = (io) => {
                   number_card_of_player[player._id] = player.cards.length;
                 });
                 room.number_turn_play++;
-                io.to(next_player.socket_id).emit(SocketConst.EMIT.NEXT_PLAYER, {next_player:next_next_player._id, before_player:player._id, card_before:room.current_field, card_of_player:next_player.cards, must_call_draw_card:checkMustCallDrawCard(room, room.room_name, next_player._id), draw_reason:DrawReason.DRAW_2, turn_right:!room.is_reverse,  number_card_play : room.number_card_play, number_turn_play : room.number_turn_play, number_card_of_player : number_card_of_player});
+                let is_must_call_draw_card = (room.current_field.special == Special.DRAW_2 || room.current_field.special == Special.WILD_DRAW_4) ? true : false;
+                io.to(next_player.socket_id).emit(SocketConst.EMIT.NEXT_PLAYER, {next_player:next_next_player._id, before_player:player._id, card_before:room.current_field, card_of_player:next_player.cards, must_call_draw_card:is_must_call_draw_card, draw_reason:DrawReason.DRAW_2, turn_right:!room.is_reverse,  number_card_play : room.number_card_play, number_turn_play : room.number_turn_play, number_card_of_player : number_card_of_player});
 
-                //saveする
                 room.save();
               }
               else if(card_play.special == Special.SKIP){
@@ -313,9 +320,9 @@ module.exports = (io) => {
                   number_card_of_player[player._id] = player.cards.length;
                 });
                 room.number_turn_play++;
-                io.to(next_player.socket_id).emit(SocketConst.EMIT.NEXT_PLAYER, {next_player:next_next_player._id, before_player:player._id, card_before:room.current_field, card_of_player:next_player.cards, must_call_draw_card:checkMustCallDrawCard(room, room.room_name, next_player._id), draw_reason:DrawReason.NOTING, turn_right:!room.is_reverse,  number_card_play : room.number_card_play, number_turn_play : room.number_turn_play, number_card_of_player : number_card_of_player});
+                let is_must_call_draw_card = (room.current_field.special == Special.DRAW_2 || room.current_field.special == Special.WILD_DRAW_4) ? true : false;
+                io.to(next_player.socket_id).emit(SocketConst.EMIT.NEXT_PLAYER, {next_player:next_next_player._id, before_player:player._id, card_before:room.current_field, card_of_player:next_player.cards, must_call_draw_card:is_must_call_draw_card, draw_reason:DrawReason.NOTING, turn_right:!room.is_reverse,  number_card_play : room.number_card_play, number_turn_play : room.number_turn_play, number_card_of_player : number_card_of_player});
 
-                //saveする
                 room.save();
               }
               else if(card_play.special == Special.REVERSE){
@@ -341,9 +348,9 @@ module.exports = (io) => {
                   number_card_of_player[player._id] = player.cards.length;
                 });
                 room.number_turn_play++;
-                io.to(next_player.socket_id).emit(SocketConst.EMIT.NEXT_PLAYER, {next_player:next_next_player._id, before_player:player._id, card_before:room.current_field, card_of_player:next_player.cards, must_call_draw_card:checkMustCallDrawCard(room, room.room_name, next_player._id), draw_reason:DrawReason.NOTING, turn_right:!room.is_reverse,  number_card_play : room.number_card_play, number_turn_play : room.number_turn_play, number_card_of_player : number_card_of_player});
+                let is_must_call_draw_card = (room.current_field.special == Special.DRAW_2 || room.current_field.special == Special.WILD_DRAW_4) ? true : false;
+                io.to(next_player.socket_id).emit(SocketConst.EMIT.NEXT_PLAYER, {next_player:next_next_player._id, before_player:player._id, card_before:room.current_field, card_of_player:next_player.cards, must_call_draw_card:is_must_call_draw_card, draw_reason:DrawReason.NOTING, turn_right:!room.is_reverse,  number_card_play : room.number_card_play, number_turn_play : room.number_turn_play, number_card_of_player : number_card_of_player});
 
-                //saveする
                 room.save();
               }
               else if(card_play.special == Special.WILD){
@@ -351,9 +358,9 @@ module.exports = (io) => {
                 socket.emit(SocketConst.EMIT.COLOR_OF_WILD, {});
 
                 //クライアントから色を受け取る
-                socket.on(SocketConst.EMIT.COLOR_OF_WILD, (data) => {
-                  room.current_field.color = data.color_of_wild;
-                });
+                // socket.on(SocketConst.EMIT.COLOR_OF_WILD, (data) => {
+                //   room.current_field.color = data.color_of_wild;
+                // });
 
                 //一定時間待ち、カードを場に出したことをクライアントに通知する
                 waitForChangeColor(room);
@@ -376,24 +383,12 @@ module.exports = (io) => {
                   number_card_of_player[player._id] = player.cards.length;
                 });
                 room.number_turn_play++;
-                io.to(next_player.socket_id).emit(SocketConst.EMIT.NEXT_PLAYER, {next_player:next_next_player._id, before_player:player._id, card_before:room.current_field, card_of_player:next_player.cards, must_call_draw_card:checkMustCallDrawCard(room, room.room_name, next_player._id), draw_reason:DrawReason.NOTING, turn_right:!room.is_reverse,  number_card_play : room.number_card_play, number_turn_play : room.number_turn_play, number_card_of_player : number_card_of_player});
+                let is_must_call_draw_card = (room.current_field.special == Special.DRAW_2 || room.current_field.special == Special.WILD_DRAW_4) ? true : false;
+                io.to(next_player.socket_id).emit(SocketConst.EMIT.NEXT_PLAYER, {next_player:next_next_player._id, before_player:player._id, card_before:room.current_field, card_of_player:next_player.cards, must_call_draw_card:is_must_call_draw_card, draw_reason:DrawReason.NOTING, turn_right:!room.is_reverse,  number_card_play : room.number_card_play, number_turn_play : room.number_turn_play, number_card_of_player : number_card_of_player});
 
-                //saveする
                 room.save();
               }
               else if(card_play.special == Special.WILD_DRAW_4){
-                //まず次のプレイヤーを取得する
-                // let next_player = getNextPlayer(room);
-                //4枚ドローする
-                // let draw_card1 = room.deck.shift();
-                // let draw_card2 = room.deck.shift();
-                // let draw_card3 = room.deck.shift();
-                // let draw_card4 = room.deck.shift();
-                //ドローしたカードを次のプレイヤーの手札に加える
-                // next_player.cards.push(draw_card1);
-                // next_player.cards.push(draw_card2);
-                // next_player.cards.push(draw_card3);
-                // next_player.cards.push(draw_card4);
                 
                 //色をクライアントに選ばせる
                 socket.emit(SocketConst.EMIT.COLOR_OF_WILD, {});
@@ -411,10 +406,6 @@ module.exports = (io) => {
                   io.sockets.in(room.room_name).emit(SocketConst.EMIT.SAY_UNO_AND_PLAY_CARD, {player:player._id, card_play:card_play, yell_uno:true});
                 }
 
-                // io.sockets.in(room.room_name).emit(SocketConst.EMIT.DRAW_CARD, {player:next_player._id, is_draw:true, can_play_card:false, reason:DrawReason.WILD_DRAW_4});
-                //次のプレイヤーにドローしたカードを通知する
-                // io.to(next_player.socket_id).emit(SocketConst.EMIT.RECEIVER_CARD, {cards_receive:[draw_card1, draw_card2, draw_card3, draw_card4],is_penalty:false});
-
                 //next_playerイベントを発火させるための処理
                 let next_next_player = getNextPlayer(room);
                 let next_player = room.players_info.find((player) => { return player._id == room.order[room.current_player]; });
@@ -423,9 +414,9 @@ module.exports = (io) => {
                   number_card_of_player[player._id] = player.cards.length;
                 });
                 room.number_turn_play++;
-                io.to(next_player.socket_id).emit(SocketConst.EMIT.NEXT_PLAYER, {next_player:next_next_player._id, before_player:player._id, card_before:room.current_field, card_of_player:next_player.cards, must_call_draw_card:checkMustCallDrawCard(room, room.room_name, next_player._id), draw_reason:DrawReason.WILD_DRAW_4, turn_right:!room.is_reverse,  number_card_play : room.number_card_play, number_turn_play : room.number_turn_play, number_card_of_player : number_card_of_player});
+                let is_must_call_draw_card = (room.current_field.special == Special.DRAW_2 || room.current_field.special == Special.WILD_DRAW_4) ? true : false;
+                io.to(next_player.socket_id).emit(SocketConst.EMIT.NEXT_PLAYER, {next_player:next_next_player._id, before_player:player._id, card_before:room.current_field, card_of_player:next_player.cards, must_call_draw_card:is_must_call_draw_card, draw_reason:DrawReason.WILD_DRAW_4, turn_right:!room.is_reverse,  number_card_play : room.number_card_play, number_turn_play : room.number_turn_play, number_card_of_player : number_card_of_player});
 
-                //saveする
                 room.save();
               }
               else if(card_play.special == Special.WILD_SHUFFLE){
@@ -482,9 +473,9 @@ module.exports = (io) => {
                   number_card_of_player[player._id] = player.cards.length;
                 });
                 room.number_turn_play++;
-                io.to(next_player.socket_id).emit(SocketConst.EMIT.NEXT_PLAYER, {next_player:next_next_player._id, before_player:player._id, card_before:room.current_field, card_of_player:next_player.cards, must_call_draw_card:checkMustCallDrawCard(room, room.room_name, next_player._id), draw_reason:DrawReason.NOTING, turn_right:!room.is_reverse,  number_card_play : room.number_card_play, number_turn_play : room.number_turn_play, number_card_of_player : number_card_of_player});
+                let is_must_call_draw_card = (room.current_field.special == Special.DRAW_2 || room.current_field.special == Special.WILD_DRAW_4) ? true : false;
+                io.to(next_player.socket_id).emit(SocketConst.EMIT.NEXT_PLAYER, {next_player:next_next_player._id, before_player:player._id, card_before:room.current_field, card_of_player:next_player.cards, must_call_draw_card:is_must_call_draw_card, draw_reason:DrawReason.NOTING, turn_right:!room.is_reverse,  number_card_play : room.number_card_play, number_turn_play : room.number_turn_play, number_card_of_player : number_card_of_player});
 
-                //saveする
                 room.save();
               }
               else if(card_play.special == Special.WHITE_WILD){
@@ -513,9 +504,9 @@ module.exports = (io) => {
                   number_card_of_player[player._id] = player.cards.length;
                 });
                 room.number_turn_play++;
-                io.to(next_player.socket_id).emit(SocketConst.EMIT.NEXT_PLAYER, {next_player:next_next_player._id, before_player:player._id, card_before:room.current_field, card_of_player:next_player.cards, must_call_draw_card:checkMustCallDrawCard(room, room.room_name, next_player._id), draw_reason:DrawReason.WHITE_WILD, turn_right:!room.is_reverse,  number_card_play : room.number_card_play, number_turn_play : room.number_turn_play, number_card_of_player : number_card_of_player});
+                let is_must_call_draw_card = (room.current_field.special == Special.DRAW_2 || room.current_field.special == Special.WILD_DRAW_4) ? true : false;
+                io.to(next_player.socket_id).emit(SocketConst.EMIT.NEXT_PLAYER, {next_player:next_next_player._id, before_player:player._id, card_before:room.current_field, card_of_player:next_player.cards, must_call_draw_card:is_must_call_draw_card, draw_reason:DrawReason.WHITE_WILD, turn_right:!room.is_reverse,  number_card_play : room.number_card_play, number_turn_play : room.number_turn_play, number_card_of_player : number_card_of_player});
 
-                //saveする
                 room.save();
               }
               else if(card_play.special == null || card_play.number != null){
@@ -538,9 +529,9 @@ module.exports = (io) => {
                   number_card_of_player[player._id] = player.cards.length;
                 });
                 room.number_turn_play++;
-                io.to(next_player.socket_id).emit(SocketConst.EMIT.NEXT_PLAYER, {next_player:next_next_player._id, before_player:player._id, card_before:room.current_field, card_of_player:next_player.cards, must_call_draw_card:checkMustCallDrawCard(room, room.room_name, next_player._id), draw_reason:DrawReason.WHITE_WILD, turn_right:!room.is_reverse,  number_card_play : room.number_card_play, number_turn_play : room.number_turn_play, number_card_of_player : number_card_of_player});
+                let is_must_call_draw_card = (room.current_field.special == Special.DRAW_2 || room.current_field.special == Special.WILD_DRAW_4) ? true : false;
+                io.to(next_player.socket_id).emit(SocketConst.EMIT.NEXT_PLAYER, {next_player:next_next_player._id, before_player:player._id, card_before:room.current_field, card_of_player:next_player.cards, must_call_draw_card:is_must_call_draw_card, draw_reason:DrawReason.WHITE_WILD, turn_right:!room.is_reverse,  number_card_play : room.number_card_play, number_turn_play : room.number_turn_play, number_card_of_player : number_card_of_player});
 
-                //saveする
                 room.save();
               }
 
