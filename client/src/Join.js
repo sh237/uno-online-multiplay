50eca810770922043394f1462c12d5636cbc7b9b
nsns245 <suginao1234@gmail.com>
2023-02-17 02:35:18 +0900

    delete code for debug

--- client/src/Join.js
+++ client/src/Join.js
@@ -32,12 +32,6 @@ const Join = () => {
             joinRoom();
         }
     }
-    //デバッグ用
-    const myCards=[{color:"red",number:0},{color:"red",special:"draw_2"}]//,{color:"blue",number:1},{color:"blue",special:"skip"},{color:"green",special:"reverse"},{special:"wild"},{color:"yellow",number:1},{color:"black",special:"wild_draw_4"},{color:"red",number:1},{color:"red",number:1},{color:"red",number:1},{color:"red",number:1},{color:"red",number:1},{color:"red",number:1},{color:"red",number:1},{color:"red",number:1},{color:"red",number:1},{color:"red",number:1},{color:"red",number:1},{color:"red",number:1},{color:"red",number:1},{color:"red",number:1},];
-    function selectCard(v){
-        console.log(v);
-    };
-    const playersCardList={1:3,2:4,3:5,4:6};
     return (
         <div className="join">
             <div className="login">
@@ -63,78 +57,6 @@ const Join = () => {
                     Join Room
                 </button>
             </div>
-            {/*デバッグ用 */}
-            {/* <div className="player1 player-card">
-                {myCards.map((v,i) => (
-                    <div onClick={() => selectCard(v)} key={i} className={`card card-hover ${v.color}`}>
-                        <div className="ellipse">
-                            {(v.number || v.number==0) && <p className="number solid-shadow">{v.number}</p>}
-                            {v.special=="draw_2" && <div>
-                                <p className={`special-${v.special}-1`}></p>
-                                <p className={`special-${v.special}-2`}></p>
-                            </div>}
-                            {v.special=="skip" && <div>
-                                <p className={`special-${v.special}-1`}></p>
-                                <p className={`special-${v.special}-2`}></p>
-                            </div>}
-                            {v.special=="reverse" && <div>
-                                <p className={`special-${v.special}-1`}></p>
-                                <p className={`special-${v.special}-2`}></p>
-                                <p className={`special-${v.special}-3`}></p>
-                                <p className={`special-${v.special}-4`}></p>
-                            </div>}
-                            {v.special=="wild_draw_4" && <div>
-                                <p className={`special-${v.special}-1 yellow`}></p>
-                                <p className={`special-${v.special}-2 blue`}></p>
-                                <p className={`special-${v.special}-3 red`}></p>
-                                <p className={`special-${v.special}-4 green`}></p>
-                            </div>}
-                            {v.special=="wild" && <div>
-                                <p className={`special-${v.special}-1 red`}></p>
-                                <p className={`special-${v.special}-2 blue`}></p>
-                                <p className={`special-${v.special}-3 yellow`}></p>
-                                <p className={`special-${v.special}-4 green`}></p>
-                            </div>}
-                            {v.special=="white_wild" && <div>
-                                <p className={`special-${v.special}`}></p>
-                            </div>}
-                            {v.special=="wild_shuffle" && <div>
-                                <p className={`special-${v.special} solid-shadow`}>shuffle</p>
-                            </div>}
-                        </div>
-                    </div>
-                ))}
-            </div> */}
-
-            {/* <div className="player2 player-card">
-                {myCards.map((v,i) => (
-                    <div className="card black" style={{marginBottom:"-50px",zIndex:i+1}}>
-                        <div className="ellipse red">
-                            <p className="logo solid-shadow">uno</p>
-                        </div>
-                    </div>
-                ))}
-            </div>
-
-            <div className="player3 player-card">
-                {myCards.map((v,i) => (
-                    <div className="card black" style={{marginBottom:"-50px",zIndex:i+1}}>
-                        <div className="ellipse red">
-                            <p className="logo solid-shadow">uno</p>
-                        </div>
-                    </div>
-                ))}
-            </div>
-
-            <div className="player4 player-card">
-                {myCards.map((v,i) => (
-                    <div className="card black" style={{marginBottom:"-50px",zIndex:i+1}}>
-                        <div className="ellipse red">
-                            <p className="logo solid-shadow">uno</p>
-                        </div>
-                    </div>
-                ))}
-            </div> */}
       </div>
     );
 }
